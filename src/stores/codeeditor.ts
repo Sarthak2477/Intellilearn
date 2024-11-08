f6745999088555892927d0751e5d8260b79b8e36
saphalpdyl <saphalpdyl@gmail.com>
2024-11-08 15:57:26 -0600

    add: create a zustand store for editor

---
+++ src/stores/codeeditor.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface CodeEditorState {
+  
+}
+
+const useCodeEditorStore = create<CodeEditorState>()(set => ({
+
+}));
+
+export default useCodeEditorStore;
