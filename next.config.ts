631ae0db22972f7a143d42210b255da62dffc8d0
saphalpdyl <saphalpdyl@gmail.com>
2024-11-12 01:00:10 -0600

    fix: dumb explanation due to double streaming

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  // Strict Mode caused some bugs when streaming chat responses
+  reactStrictMode: false,
 };
 
 export default nextConfig;
--- src/actions/explanation-generator.ts
+++ src/actions/explanation-generator.ts
@@ -9,17 +9,25 @@ export async function generateExplanationFromSchema(schema: string, explanationP
     apiKey: process.env.OPENAI_API_KEY,
   });
 
+  // console.log(schema);
+  // console.log(`Overall SQL \n \`\`\`sql${schema}\`\`\` \n\n\n Section I want to know about:\n \`\`\`${explanationPart}\`\`\``);
+  
   const stream = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
       ...engineeredPrompt as ChatCompletionMessageParam[],
       {
         role: "user",
-        content: `SQL \n \`\`\`sql${schema}\`\`\` \n Section: \`\`\`${explanationPart}\`\`\``
+        content: [
+          {
+            type: "text",
+            text: `Overall SQL \n \`\`\`sql${schema}\`\`\` \n\n\n Section I want to know about:\n \`\`\`${explanationPart}\`\`\``,
+          }
+        ]
       }
     ],
-    temperature: 0.4,
-    max_tokens: 1000,
+    temperature: 1,
+    max_tokens: 2048,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 1.5,
@@ -28,6 +36,8 @@ export async function generateExplanationFromSchema(schema: string, explanationP
     },
     stream: true,
   });
+
+  // console.log(stream.choices[0]);
 
   return stream;
 }
