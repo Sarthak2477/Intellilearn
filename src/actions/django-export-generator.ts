6214b863b577a2cf5d85919db096644850e750af
saphalpdyl <saphalpdyl@gmail.com>
2024-11-12 04:29:18 -0600

    add: django export

---
+++ src/actions/django-export-generator.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import OpenAI from "openai";
+
+import engineeredPrompt from "@/prompts/prompt-django";
+import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+
+export async function generateDjangoModelFromSchema(schema: string) {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      ...engineeredPrompt as ChatCompletionMessageParam[],
+      {
+        "role": "user",
+        "content": `Generate django models based on this schema: ${schema}`
+      }
+    ],
+    temperature: 1,
+    max_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    response_format: {
+      "type" : "text"
+    },
+    stream: true,
+  });
+
+  return response;
+}
--- src/components/board/components/inspector/export.tsx
+++ src/components/board/components/inspector/export.tsx
@@ -1,17 +1,45 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import React, { FormEvent, useState } from 'react'
+import { ChatCompletionChunk } from 'openai/resources/index.mjs';
+import { Stream } from 'openai/streaming.mjs';
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import useInspectorStore from '@/stores/inspector';
+import { generateDjangoModelFromSchema } from '@/actions/django-export-generator';
+import MonacoEditor from "@monaco-editor/react";
 
 type ExportOptions = "django" | "prisma" | "eloquent" | "drizzle";
 
 export default function ExportSchemaToORMSection() {
+  const [ exporting, setExporting ] = useState(false);
   const [ output, setOutput ] = useState("");
   const [ exportOption, setExportOption ] = useState<ExportOptions>("django");
+
+  const { mainSchemaText } = useInspectorStore();
   
   const handleChangeExportOption = (option: ExportOptions) => setExportOption(option);
+
+  async function handleExport(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if ( exporting ) return;
+
+    setOutput("");
+    setExporting(true);
+    
+    if ( exportOption === "django" ) {
+      const stream = await generateDjangoModelFromSchema(mainSchemaText);
+      
+      for await (const chunk of stream as Stream<ChatCompletionChunk>) {
+        const data = chunk.choices[0]?.delta.content;
+        setOutput(explanation => explanation + (data !== undefined ? data : ""));
+      }
+    }
+    setExporting(false);
+  }
   
   return (
-    <div>
-      <form onSubmit={() => {}} className='flex items-center gap-2'>
+    <div className='space-y-2'>
+      <form onSubmit={handleExport} className='flex items-center gap-2'>
           <Select value={exportOption} onValueChange={handleChangeExportOption}>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="Export Option" />
@@ -24,9 +52,39 @@ export default function ExportSchemaToORMSection() {
             </SelectContent>
           </Select>
           <button 
+            disabled={exporting}
             className='px-3 py-2 rounded-lg bg-emerald-600 hover:bg-emerald-700 text-white font-semibold text-sm'
           >{output ? "Regenerate" : "Generate"}</button>
         </form>
+      <MonacoEditor 
+        value={output.replace(/```[a-zA-Z0-9]+\n|\n```/g, '').replace(/```/,"")}
+        className="h-[70vh]"
+        language={
+          exportOption === "django" ? "python" :
+          exportOption === "prisma" ? "prisma" :
+          "javascript"
+        }
+        theme="custom-theme"
+        options={{
+          minimap: {
+            enabled: false,
+          },
+          fontFamily: "JetBrains Mono",
+          fontSize: 12,
+        }}
+        beforeMount={monaco => {
+          monaco.editor.defineTheme('custom-theme', {
+            base: 'vs-dark',
+            inherit: true,
+            rules: [],
+            colors: {
+              'editor.background': '#00000000',
+            },
+          });
+
+        }}
+        keepCurrentModel={true}
+      />
     </div>    
   );
 }
